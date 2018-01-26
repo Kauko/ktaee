d0c618aa04ab0be47411f0ad6f7fead630a1fe14
Antti Liimatta <1601177@edu.karelia.fi>
2018-01-26 21:55:27 +0200

    Skene

---
+++ Assets/TriggerEnabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnabler : MonoBehaviour {
+
+    public BoxCollider activatorCol;
+	
+	void Start ()
+    {
+		
+	}
+
+    void Update ()
+    {
+		if (Input.GetKey(KeyCode.E) && activatorCol.enabled == false)
+        {
+            activatorCol.enabled = true;
+        }
+        else
+        {
+            activatorCol.enabled = false;
+        }
+	}
+}
